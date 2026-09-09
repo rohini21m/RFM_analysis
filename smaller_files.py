d08badec7b122e7d1979fb1d92e62c836e681a79
rohini21m <67290441+rohini21m@users.noreply.github.com>
2026-09-09 15:45:27 -0500

    Improve comments in smaller_files.py
    Added a period to the comment for clarity and included a new comment about Snowflake operations.

--- smaller_files.py
+++ smaller_files.py
@@ -1,4 +1,4 @@
-# splitting a huge file into smaller chunks to load into snowflake 
+# splitting a huge file into smaller chunks to load into snowflake. 
 
 
 import pandas as pd 
@@ -8,4 +8,6 @@ for chunk in pd.read_csv("path/fact_transactions",chunksize = chunk_size):
     chunk.to_csv('fact_transactions' + str(batch_no) + '.csv', index=False) 
     batch_no+=1 
 
+
+# snowflake : create tables,user_stage-> user_stage->copy_into command 
 
