55e4974d3d981832bcde6d5c686492928f256764
rohini21m <67290441+rohini21m@users.noreply.github.com>
2026-09-09 15:43:51 -0500

    Add script to split large file into smaller chunks to load files into snowflake.
    Loaded sample files into user stage and then copied to snowflake tables.

---
+++ smaller_files.py
@@ -0,0 +1,11 @@
+# splitting a huge file into smaller chunks to load into snowflake 
+
+
+import pandas as pd 
+chunk_size=275039
+batch_no=1
+for chunk in pd.read_csv("path/fact_transactions",chunksize = chunk_size): 
+    chunk.to_csv('fact_transactions' + str(batch_no) + '.csv', index=False) 
+    batch_no+=1 
+
+
